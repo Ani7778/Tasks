1d21179168138bc683beb644e84e0142fac6ae46
Ani7778 <ani_asatryan_2000@mail.ru>
2021-04-29 17:24:12 +0400

    Get data using promise

--- getData.js
+++ getData.js
@@ -1,13 +1,23 @@
-const fetch = require("node-fetch");
+const axios = require('axios')
 
 let url = "https://random-data-api.com/api/users/random_user";
 
 async function getResponse() {
-    let response = await fetch(url);
-    let content = await response.json()
+    let response = await axios.get(url);
+    let content = await response.data
 
     return content;
 }
 
 getResponse();
+
+
+axios.get(url)
+.then(function (response) {
+    return response.data
+})
+.catch(function (err)  {
+    console.error("Failed", err);
+})
+
 
